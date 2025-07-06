e1abb4b77ef4e0d72c48a82f342ce3c01212813e
Ben Turner <ext.ben.turner@nomadrail.com>
2025-07-06 23:20:08 +0200

    simple application to create a ball bouncing from left to right

--- pingpong.asm
+++ pingpong.asm
@@ -1,9 +1,33 @@
+; === simple application to create a ball bouncing from left to right
 
 START:
-    LDA #$0D                        ; green
-    LDX #$1F                        ; to fill one line we need to iterate 32 times
-LINEFILL:
-    STA $0400,X
-    DEX                             ; decrease X
-    BPL LINEFILL
+    LDA #$0D                        ; load green into the accumulator
+    LDX #$00                        ; index used for left-right
+    STA $0400,X                     ; colour the first pixel before we start
+RIGHT:
+    LDA #$00                        ; load black into the accumulator
+    STA $0400,X                     ; write black to current index location
+    INX                             ; increase x (next pixel to the right)
+    LDA #$0D                        ; load green into the accumulator
+    STA $0400,X                     ; write green to current index location
+    JSR DELAY                       ; call the DELAY subroutine to burn some CPU cycles
+    CPX #$1F                        ; compare X register to 1F, video output is 32 pixels wide
+    BNE RIGHT                       ; halt loop once 1F has been reached, otherwise repeat RIGHT
+LEFT:
+    LDA #$00                        ; load black into the accumulator
+    STA $0400,X                     ; write black to current index location
+    DEX                             ; decrease x (next pixel to the left)
+    LDA #$0D                        ; load green into the accumulator
+    STA $0400,X                     ; write green to current index location
+    JSR DELAY                       ; call the DELAY subroutine to burn some CPU cycles
+    CPX #$00                        ; compare X register to 00, meaning we're on the left side of the video
+    BNE LEFT                        ; halt loop once 00 has been reached, otherwise repeat LEFT
+    
+    JMP RIGHT                       ; loop forever! :)
 
+; === Simple delay subroutine that just burns some cpu cycles ===
+DELAY:
+    LDY #$30
+D1: DEY
+    BNE D1
+    RTS
