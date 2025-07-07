89a44d6d8c9f0d7ecd2554c9b161a0f779f7f79b
Ben Turner <ext.ben.turner@nomadrail.com>
2025-07-07 10:49:57 +0200

    refactor right and left into one subroutine

--- pingpong.asm
+++ pingpong.asm
@@ -7,27 +7,35 @@ START:
     LDX #$00                        ; index used for left-right
     STA $0400,X                     ; colour the first pixel before we start
 RIGHT:
-    LDA #$00                        ; load black into the accumulator
-    STA $0400,X                     ; write black to current index location
-    INX                             ; increase x (next pixel to the right)
-    LDA $00                         ; load current colour into the accumulator
-    STA $0400,X                     ; write current colour to current index location
-    JSR DELAY                       ; call the DELAY subroutine to burn some CPU cycles
-    CPX #$1F                        ; compare X register to 1F, video output is 32 pixels wide
-    BNE RIGHT                       ; halt loop once 1F has been reached, otherwise repeat RIGHT
-    INC $00                         ; bump our colour by one
+    LDA #$01                        ; we're moving right (+1)
+    STA $01                         ; store direction at $01
+    LDA #$1F                        ; right edge
+    STA $02                         ; store our right edge at $02
+    JSR MOVEBALL
 LEFT:
+    LDA #$FF                        ; we're moving left (-1)
+    STA $01                         ; store direction at $01
+    LDA #$00                        ; right edge
+    STA $02                         ; store our left edge at $02
+    JSR MOVEBALL
+REPEAT:
+    JMP RIGHT                       ; loop forever! :)
+
+; === Subroutine to move the ball in the right direction ===
+MOVEBALL:
     LDA #$00                        ; load black into the accumulator
     STA $0400,X                     ; write black to current index location
-    DEX                             ; decrease x (next pixel to the left)
+    TXA                             ; transfer x register to accumulator
+    CLC                             ; clear carry
+    ADC $01                         ; $01 contains our direction
+    TAX                             ; transfer accumulator back to x
     LDA $00                         ; load current colour into the accumulator
     STA $0400,X                     ; write current colour to current index location
     JSR DELAY                       ; call the DELAY subroutine to burn some CPU cycles
-    CPX #$00                        ; compare X register to 00, meaning we're on the left side of the video
-    BNE LEFT                        ; halt loop once 00 has been reached, otherwise repeat LEFT
+    CPX $02                         ; compare X register to $02 which contains boundary, video output is 32 pixels wide
+    BNE MOVEBALL                    ; halt loop once 1F has been reached, otherwise repeat MOVEBALL
     INC $00                         ; bump our colour by one
-
-    JMP RIGHT                       ; loop forever! :)
+    RTS
 
 ; === Simple delay subroutine that just burns some cpu cycles ===
 DELAY:
